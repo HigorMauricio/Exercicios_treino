fc194d1d989da123e90766a27ca916caa23584b2
HigorMauricio <higormauriciosilva@gmail.com>
2025-05-04 13:31:20 -0300

    commit ex06

---
+++ ex06/ex06.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int numero;
+
+    bool eh_perfeito(int numero);
+
+    cout << "Informe um numero positivo: ";
+    cin >> numero;
+    if (eh_perfeito(numero)){
+        cout << "eh perfeito";
+    } else {
+        cout << "nao eh perfeito";
+    }
+}
+
+bool eh_perfeito(int numero){
+    int soma = 0;
+    for (int i = 1; i < numero; i++){
+        if (numero % i == 0){
+            soma += i;
+        }
+    }
+    return numero == soma;
+}
